638ab55cb835743c483ac9626edd8e3dce6a3414
Jacob LeGrone <git@jacoblegrone.com>
2017-07-15 15:41:56 -0400

    chore(repo): sync commitizen and validate-commit-msg configs
    validate-commit-msg configuration from package.json is now used in
    cz-config to ensure that valid types and scopes are kept in sync

--- .cz-config.js
+++ .cz-config.js
@@ -1,57 +1,57 @@
-'use strict';
+const { config } = require('./package.json')
+
+const { types: typeKeys, scope } = config['validate-commit-msg']
+const typeSet = new Set(typeKeys)
+
+const types = [
+  {
+    value: 'feat',
+    name: 'feat:     A new feature'
+  },
+  {
+    value: 'fix',
+    name: 'fix:      A bug fix'
+  },
+  {
+    value: 'docs',
+    name: 'docs:     Documentation only changes'
+  },
+  {
+    value: 'style',
+    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
+  },
+  {
+    value: 'refactor',
+    name: 'refactor: A code change that neither fixes a bug nor adds a feature'
+  },
+  {
+    value: 'perf',
+    name: 'perf:     A code change that improves performance'
+  },
+  {
+    value: 'test',
+    name: 'test:     Adding missing tests'
+  },
+  {
+    value: 'chore',
+    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
+  },
+  {
+    value: 'revert',
+    name: 'revert:   Revert to a commit'
+  },
+  {
+    value: 'WIP',
+    name: 'WIP:      Work in progress'
+  }
+].filter(type => typeSet.has(type.value))
+
+const scopes = scope.allowed.map(name => ({ name }))
 
 module.exports = {
 
-  types: [
-    {
-      value: 'feat',
-      name: 'feat:     A new feature'
-    },
-    {
-      value: 'fix',
-      name: 'fix:      A bug fix'
-    },
-    {
-      value: 'docs',
-      name: 'docs:     Documentation only changes'
-    },
-    {
-      value: 'style',
-      name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
-    },
-    {
-      value: 'refactor',
-      name: 'refactor: A code change that neither fixes a bug nor adds a feature'
-    },
-    {
-      value: 'perf',
-      name: 'perf:     A code change that improves performance'
-    },
-    {
-      value: 'test',
-      name: 'test:     Adding missing tests'
-    },
-    {
-      value: 'chore',
-      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
-    },
-    {
-      value: 'revert',
-      name: 'revert:   Revert to a commit'
-    },
-    {
-      value: 'WIP',
-      name: 'WIP:      Work in progress'
-    }
-  ],
-
-  scopes: [
-    {name: 'macos'},
-    {name: 'mobile'},
-    {name: 'redux-lib'},
-    {name: 'web'},
-    {name: 'repo'}
-  ],
+  types,
+  scopes,
 
   // it needs to match the value for field type. Eg.: 'fix'
   /*
@@ -68,4 +68,4 @@ module.exports = {
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix', 'perf', 'refactor']
 
-};
+}
